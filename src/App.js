848459e20ba96ef6bf03214697c4b93f541fdcc1
jared <jaredrotich3@gmail.com>
2025-04-26 00:23:01 +0300

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm JARED</h1>
+      <img
+      src=""
+       alt="Photo of Jared smiling"
+      />
+      <h2>About Me</h2>
+
+      <p>
+         I'm a passionate developer who loves building and doing deep research.
+       </p>
+       <a href="https://github.com/dashboard" >
+        Github 
+       </a>
+        <br />
+        <a href="https://www.linkedin.com/mynetwork/grow/">
+         LinkedIn
+        </a>
+    </div>
+  );
 }
 
 export default App;
